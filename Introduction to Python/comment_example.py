dda9636bf2027810ef7f9eea037c10acabf45a07
Mahmish <159629870+Mahmish@users.noreply.github.com>
2025-05-18 11:23:53 +0500

    Add files via upload

---
+++ Introduction to Python/comment_example.py
@@ -0,0 +1,23 @@
+# This is a Python script to demonstrate comments
+
+# Assigning a string to a variable
+my_string = "This is a string"  # This is an inline comment
+
+# Printing the string to the console
+print(my_string)
+
+# Performing a mathematical operation
+result = 5 + 10  # Adding two numbers
+
+# Printing the result
+print("The result is:", result)
+
+"""
+This is a multi-line comment.
+It can be used to provide more detailed explanations
+or to temporarily disable blocks of code.
+For example, the following code will not be executed:
+# print("This line is commented out")
+"""
+
+# End of the script
--- Introduction to Python/greeting.py
+++ Introduction to Python/greeting.py
@@ -1,1 +1,5 @@
+# This script prints a greeting with my name
+name = "Mahmish Munawar"
+greeting = "Hello"
+print(greeting + ", " + name + "!")
 
