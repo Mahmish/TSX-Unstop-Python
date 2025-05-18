b092a3637c05e6c2730204429b5f59d4e47d3d5d
Mahmish <159629870+Mahmish@users.noreply.github.com>
2025-05-18 11:51:13 +0500

    Delete Basic_Dataypes_and_Operators directory

--- Basic_Dataypes_and_Operators/string_concat.py
+++
@@ -1,9 +0,0 @@
-# This script concatenates two strings
-string1 = "Hello"
-string2 = "World"
-
-# Concatenate the strings
-result = string1 + " " + string2  # Add a space in between
-
-# Print the result
-print("The concatenated string is:", result)
