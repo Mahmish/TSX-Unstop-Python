5f99cbd5abc55c0df7d94f0ddc5e4a9d2bdb3c36
Mahmish <159629870+Mahmish@users.noreply.github.com>
2025-05-18 11:52:59 +0500

    Add files via upload

---
+++ Basic_Dataypes_and_Operators/string_concat.py
@@ -0,0 +1,9 @@
+# This script concatenates two strings
+string1 = "Hello"
+string2 = "World"
+
+# Concatenate the strings
+result = string1 + " " + string2  # Add a space in between
+
+# Print the result
+print("The concatenated string is:", result)
