ba84b7419ceabbc68eece37f42ec002774c95c74
Mahmish <159629870+Mahmish@users.noreply.github.com>
2025-05-18 11:47:40 +0500

    Basic_Dataypes_and_Operators/aritmrticOperators

--- Basic_Dataypes_and_Operators/leap_year.py
+++
@@ -1,8 +0,0 @@
-# This script checks if a year is a leap year
-year = int(input("Enter a year: "))  # Get the year from the user
-
-# Check if it's a leap year
-if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-    print(year, "is a leap year.")
-else:
-    print(year, "is not a leap year.")
